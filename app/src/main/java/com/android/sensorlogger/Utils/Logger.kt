a0cb381a564a7051d0356901e688115e7ced5d04
Julian <dreerr@gmail.com>
2020-10-09 00:46:26 +0200

    Logger improvements

--- app/src/main/java/com/android/sensorlogger/Utils/Logger.kt
+++ app/src/main/java/com/android/sensorlogger/Utils/Logger.kt
@@ -6,21 +6,18 @@ import com.android.sensorlogger.App
 import java.io.BufferedWriter
 import java.io.File
 import java.nio.charset.Charset
-import java.text.SimpleDateFormat
 import java.util.*
 
 open class Logger(open var context: Context, var fileNameTag : String) {
-    private lateinit var logFile : File
+    private var logFile : File? = null
     private var bufferedWriter: BufferedWriter? = null
     private var lastCreated = 0L
 
-
     private fun initNewFile(){
         logFile = Util.getFile(fileNameTag, "txt")
-        logFile = File(App.storage, fileNameTag + SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.US).format(Date()) + ".txt")
-        bufferedWriter = logFile.bufferedWriter(Charset.defaultCharset(), 16 * 1024)
+        bufferedWriter = logFile?.bufferedWriter(Charset.defaultCharset(), 16 * 1024)
         lastCreated = Date().time
-        Log.d(TAG, "Created new file ${logFile.name}")
+        Log.d(TAG, "Created new file ${logFile?.name}")
     }
 
     fun writeLine(line : String) {
@@ -35,12 +32,13 @@ open class Logger(open var context: Context, var fileNameTag : String) {
     }
 
     fun closeFile(){
+        if(logFile==null) return
         bufferedWriter?.close()
-        Log.d(TAG, "Closed file ${logFile.name}")
-        if (logFile.length() > 0) {
-            App.uploadManager.add(logFile)
+        Log.d(TAG, "Closed file ${logFile?.name}")
+        if (logFile!!.length() > 0) {
+            App.uploadManager.add(logFile!!)
         } else {
-            logFile.delete()
+            logFile?.delete()
         }
     }
 }
